63979544caf6353ed7e4024e08b55227ba2bbf8a
Sapinderjit <er.sapinder.cs@gmail.com>
2023-06-30 13:01:25 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App=() =>{
   const[user,setUser]=useState('');
   const[email,setEmail]=useState('');
   const[password,setPassword]=useState('');
-  const[emailError,setEmailError]=useState('1');
+  const[emailError,setEmailError]=useState('');
   const[passwordError,setPasswordError]=useState('');
   const[hasAccount,setHasAccount]=useState(false);
 
